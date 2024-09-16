691e0bfecb2d78a16a33ef39553acad810cd76d3
luangst <luisaangstmoncalves13@gmail.com>
2024-09-16 11:53:12 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+"1° atividade"
+/*let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+// 1° console (10), 2° console (10,5)
+
+
+let a = 10
+let b = 20
+c = a  //a vale 10
+b = c  // " = 10 tmb 
+a = b  // " = 10 
+
+console.log(a, b, c)
+
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
+//vai aparecer a divisao do {t/p}*/
+
+
+"2° atividade"
+/*var nome = prompt ("Qual é seu nome?")
+var idade = prompt ("Qual a sua idade?")
+
+//console.log (typeof nome ,typeof idade )
+ //(apareceu string string (tipo:texto) apareceu porque foi o tipo que eu pedi
+
+const nome = prompt ("Qual é seu nome?")
+const idade = prompt ("Qual a sua idade?")
+
+//console.log("Qual é seu nome?", nome, "Qual a sua idade?", idade)
+// apareceu a resposta que o usuário deu
+
+console.log("Olá",nome,"voce tem", idade,"anos")*/
+
+
+
+/*"Você está usando brinco hoje?" -"sim"
+"Você está usando alguma roupa rosa hoje?" - "não"
+"Você está usando tênis hoje?" - "sim"*/
+
+
+
+let a = 10             
+let b = 25
+let c 
+c=b
+b=a
+//console.log (c , a) 
+
+a=c
+c=
+
+console.log (a , b)
+
